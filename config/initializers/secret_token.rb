0cf83b6696602c7b7fb53ae5de6ee55ed6f50968
Bastian Albers <baalbers@uos.de>
2012-09-21 23:47:56 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookshelf::Application.config.secret_token = '7dfe7d7af6364dec6225059cb8fa12b51ed5efce9c5ff1bae2838d319a5fa7e79386cbdf6888cce14de8db16c1a8c9a9a7f8b8eb70d5a6f88523d7fcc8370795'
